f2a7a221eee37d4cb349070cbd08c9d177b72b8b
lindionez <lindionezmacedo@gmail.com>
2022-07-26 22:18:19 -0300

    Adicionado: Pesquisar todos, login, delete e key chefe

--- dataBase/controlers/users.js
+++ dataBase/controlers/users.js
@@ -1,4 +1,5 @@
 const connection = require('../mysql');
+const { key } = require('../config.json');
 
 module.exports = {
     async insert(req, res) {
@@ -18,7 +19,6 @@ module.exports = {
     },
     async update(req, res) {
         var id = req.params.id;
-
         var data = {
             "Nome": req.body.name,
             "Email": req.body.email,
@@ -32,5 +32,38 @@ module.exports = {
             res.json({ status : "error"});
         }
     },
-    
+    async findAll(req, res) {
+        try {
+            var respose = await connection.query('SELECT * FROM users');
+            res.json(respose[0]);
+        } catch (error) {
+            console.log(error);
+            res.json({ status : "error"});
+        }
+    },
+    async login(req, res) {
+        var nome = req.params.nome;
+        var senha = req.params.senha;
+        try {
+            var respose = await connection.query('SELECT * FROM users WHERE nome = ?', [nome]);
+            if (respose[0][0].senha != senha) return await res.json({ error : "senha incorreta"});
+            await res.json(respose[0]);
+        } catch (error) {
+            console.log(error);
+            res.json({ status : "error"});
+        }
+    },
+    async delete(req, res) {
+        var nome = req.params.nome;
+        var token = req.params.token;
+        try {
+            var respose = await connection.query('DELETE FROM users WHERE nome = ?', [nome]);
+            if (respose[0].affectedRows == 0) return await res.json({ error : "usuario não encontrado"});
+            if (token != key) return await res.json({ error : "token incorreto"});
+            res.json(respose);
+        } catch (error) {
+            console.log(error);
+            res.json({ status : "error"});
+        }
+    }
 }
--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const users = require('./dataBase/controlers/users');
 app.use(express.json());
 
 // Rotas
-app.post('/database/usuarios', users.insert);
-app.put('/database/update/:id', users.update);
+app.post('/database/usuarios/adicionar', users.insert);
+app.put('/database/usuarios/update/:id', users.update);
+app.get('/database/usuarios/getall', users.findAll);
+app.get('/database/usuarios/getbyname/name=:nome&&senha=:senha', users.login);
+app.delete('/database/usuarios/delete/token=:token&&name=:nome', users.delete);
 
 
 // Inicializando o servidor
